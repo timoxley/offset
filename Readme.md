f34f6721fbd647d31cc88c74293d282178f0c9b8
Tim Oxley <secoif@gmail.com>
2012-10-25 15:16:39 +1000

    Initial Commit.

---
+++ Readme.md
@@ -0,0 +1,16 @@
+
+# dom-offset
+
+  Get offset of a dom element.
+
+## Installation
+
+    $ component install timoxley/dom-offset
+
+## API
+
+   
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,59 @@
+var support = require('')
+var rroot = /^(?:body|html)$/i;
+
+jQuery.fn.offset = function( options ) {
+	if ( arguments.length ) {
+		return options === undefined ?
+			this :
+			this.each(function( i ) {
+				jQuery.offset.setOffset( this, options, i );
+			});
+	}
+
+	var docElem, body, win, clientTop, clientLeft, scrollTop, scrollLeft,
+		box = { top: 0, left: 0 },
+		elem = this[ 0 ],
+		doc = elem && elem.ownerDocument;
+
+	if ( !doc ) {
+		return;
+	}
+
+	if ( (body = doc.body) === elem ) {
+		return jQuery.offset.bodyOffset( elem );
+	}
+
+	docElem = doc.documentElement;
+
+	// Make sure it's not a disconnected DOM node
+	if ( !jQuery.contains( docElem, elem ) ) {
+		return box;
+	}
+
+	// If we don't have gBCR, just use 0,0 rather than error
+	// BlackBerry 5, iOS 3 (original iPhone)
+	if ( typeof elem.getBoundingClientRect !== "undefined" ) {
+		box = elem.getBoundingClientRect();
+	}
+	win = getWindow( doc );
+	clientTop  = docElem.clientTop  || body.clientTop  || 0;
+	clientLeft = docElem.clientLeft || body.clientLeft || 0;
+	scrollTop  = win.pageYOffset || docElem.scrollTop;
+	scrollLeft = win.pageXOffset || docElem.scrollLeft;
+	return {
+		top: box.top  + scrollTop  - clientTop,
+		left: box.left + scrollLeft - clientLeft
+	};
+};
+
+function bodyOffset( body ) {
+	var top = body.offsetTop,
+		left = body.offsetLeft;
+
+	if ( support.doesNotIncludeMarginInBodyOffset ) {
+		top  += parseFloat( jQuery.css(body, "marginTop") ) || 0;
+		left += parseFloat( jQuery.css(body, "marginLeft") ) || 0;
+	}
+
+	return { top: top, left: left };
+}
