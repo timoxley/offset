94e41f09483f8920b7cc63babc61a44780f7b999
Tim Oxley <secoif@gmail.com>
2012-12-01 15:56:25 +0800

    Tidy code.

--- index.js
+++ index.js
@@ -6,54 +6,48 @@ var contains = require('within-document')
  *
  * @api public
  */
+
 module.exports = function offset(el) {
-	var box = { top: 0, left: 0 }
   var doc = el && el.ownerDocument
+  if (!doc) return
 
-	if (!doc) {
-    console.warn('no document!')
-		return
-	}
-
-	// Make sure it's not a disconnected DOM node
-	if (!contains(el)) {
-		return box
-	}
+  // Make sure it's not a disconnected DOM node
+  if (!contains(el)) return box
 
   var body = doc.body
-	if (body === el) {
-		return bodyOffset(el)
-	}
+  if (body === el) {
+    return bodyOffset(el)
+  }
 
-	var docEl = doc.documentElement
+  var box = { top: 0, left: 0 }
+  if ( typeof el.getBoundingClientRect !== "undefined" ) {
+    // If we don't have gBCR, just use 0,0 rather than error
+    // BlackBerry 5, iOS 3 (original iPhone)
+    box = el.getBoundingClientRect()
+  }
 
-	// If we don't have gBCR, just use 0,0 rather than error
-	// BlackBerry 5, iOS 3 (original iPhone)
-	if ( typeof el.getBoundingClientRect !== "undefined" ) {
-		box = el.getBoundingClientRect()
-	}
+  var docEl = doc.documentElement
+  var clientTop  = docEl.clientTop  || body.clientTop  || 0
+  var clientLeft = docEl.clientLeft || body.clientLeft || 0
+  var scrollTop  = window.pageYOffset || docEl.scrollTop
+  var scrollLeft = window.pageXOffset || docEl.scrollLeft
 
-	var clientTop  = docEl.clientTop  || body.clientTop  || 0
-	var clientLeft = docEl.clientLeft || body.clientLeft || 0
-	var scrollTop  = window.pageYOffset || docEl.scrollTop
-	var scrollLeft = window.pageXOffset || docEl.scrollLeft
-
-	return {
-		top: box.top  + scrollTop  - clientTop,
-		left: box.left + scrollLeft - clientLeft
-	}
+  return {
+    top: box.top  + scrollTop  - clientTop,
+    left: box.left + scrollLeft - clientLeft
+  }
 }
 
 function bodyOffset(body) {
-	var top = body.offsetTop
-	var left = body.offsetLeft
+  var top = body.offsetTop
+  var left = body.offsetLeft
 
-	if (support.doesNotIncludeMarginInBodyOffset) {
-		top  += parseFloat(body.style.marginTop || 0)
-		left += parseFloat(body.style.marginLeft || 0)
-	}
+  if (support.doesNotIncludeMarginInBodyOffset) {
+    top  += parseFloat(body.style.marginTop || 0)
+    left += parseFloat(body.style.marginLeft || 0)
+  }
 
-	return {
+  return {
     top: top,
     left: left
   }
