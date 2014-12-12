ae5202e765789754a6ec93124692966d4148efb8
Nathan Rajlich <nathan@tootallnate.net>
2014-12-11 18:18:13 -0800

    index: add support for "collapsed" Range instances

--- index.js
+++ index.js
@@ -27,6 +27,23 @@ module.exports = function offset(el) {
     // If we don't have gBCR, just use 0,0 rather than error
     // BlackBerry 5, iOS 3 (original iPhone)
     box = el.getBoundingClientRect()
+
+    if (el.collapsed && box.left === 0 && box.top === 0) {
+      // collapsed Range instances sometimes report 0, 0
+      // see: http://stackoverflow.com/a/6847328/376773
+      var span = doc.createElement("span");
+
+      // Ensure span has dimensions and position by
+      // adding a zero-width space character
+      span.appendChild(doc.createTextNode("\u200b"));
+      el.insertNode(span);
+      box = span.getBoundingClientRect();
+
+      // Remove temp SPAN and glue any broken text nodes back together
+      var spanParent = span.parentNode;
+      spanParent.removeChild(span);
+      spanParent.normalize();
+    }
   }
 
   var docEl = doc.documentElement
